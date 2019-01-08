c1587a607ca1b5e21568ec8da3b834efc4fcca80
Can Bayar <canbayar91@gmail.com>
2019-01-08 23:27:47 +0300

    Unit Test Framework

---
+++ UnitTests/EvaluationFunctionsTest.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests {
+
+	TEST_CLASS(GeometricFunctionsTest) {
+	public:
+
+		TEST_METHOD(calculateScaledJacobianTest) {
+			Assert::AreEqual(1, 1);	
+		}
+
+	};
+}
---
+++ UnitTests/GeometricFunctionsTest.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests {	
+
+	TEST_CLASS(GeometricFunctionsTest) {
+	public:
+		
+		TEST_METHOD(dotProductTest) {
+			Assert::AreEqual(1, 1);
+		}
+
+	};
+}
